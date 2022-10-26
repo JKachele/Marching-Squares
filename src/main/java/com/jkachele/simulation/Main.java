99dd78c1c7b5089b81c8655770e6447e0b5fc900
Justin Kachele <justin@kachele.com>
2022-10-26 14:26:10 -0400

    initial commit

---
+++ src/main/java/com/jkachele/simulation/Main.java
@@ -0,0 +1,11 @@
+/******************************************
+ *Project-------Marching-Squares
+ *File----------Main.java
+ *Author--------Justin Kachele
+ *Date----------10/26/2022
+ *License-------MIT License
+ ******************************************/
+package com.jkachele.simulation;
+
+public class Main {
+}
